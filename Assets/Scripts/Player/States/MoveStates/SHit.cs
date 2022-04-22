0f5dff74807716dfdc0b6153c8f40e5e2d05716a
IsaacBustad <ijbbustad@gmail.com>
2022-04-22 10:41:15 -0500

    refac
    refax

--- Assets/Scripts/Player/States/MoveStates/SHit.cs
+++ Assets/Scripts/Player/States/MoveStates/SHit.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -53,7 +53,7 @@ public class SHit : IMoveState
     }
     public float MaxSpeed()
     {
-        return 12;
+        return 36;
     }
     public string CurMoveState()
     {
